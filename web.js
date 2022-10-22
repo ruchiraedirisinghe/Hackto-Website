7612d770792b0eda7c84577bae5f1e733d42f17f
SurajiEkanayaka <surajiekanayaka4@gmail.com>
2022-10-22 20:31:24 +0530

    Registration Form

---
+++ web.js
@@ -0,0 +1,11 @@
+function validate(){
+	var fullname=document.form1.fName.value;
+	var add=document.form1.address.value.length;
+
+	if(fullname=="" ){
+		alert("Name cannot be empty");
+	}
+	if(add.length<10){
+		
+	}
+}
